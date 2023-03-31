8a41a446bdd5223021d366a6b941eccb205dfbf0
mehzabin-haque <bsse1233@iit.du.ac.bd>
2023-04-01 02:26:25 +0600

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { QuestionComponent } from './question/question.component';
+import { ResultComponent } from './result/result.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    QuestionComponent,
+    ResultComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './src/**/*.html',
+        './src/**/*.ts'
+    ],
+    theme: {},
+    variants: {},
+    plugins: []
+};
